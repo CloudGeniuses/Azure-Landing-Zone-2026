5a9354dc0a7b6dcda3488c945c3a204f984e2dc4
CloudGeniuses <132877816+CloudGeniuses@users.noreply.github.com>
2026-06-05 17:06:09 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+#################################################
+# ADIRYX ROOT MANAGEMENT GROUP
+#################################################
+
+resource "azurerm_management_group" "adiryx" {
+  name         = "adiryx"
+  display_name = "Adiryx"
+}
+
+#################################################
+# PLATFORM
+#################################################
+
+resource "azurerm_management_group" "platform" {
+  name                       = "adiryx-platform"
+  display_name               = "Adiryx Platform"
+  parent_management_group_id = azurerm_management_group.adiryx.id
+}
+
+resource "azurerm_management_group" "identity" {
+  name                       = "adiryx-identity"
+  display_name               = "Adiryx Identity"
+  parent_management_group_id = azurerm_management_group.platform.id
+}
+
+resource "azurerm_management_group" "connectivity" {
+  name                       = "adiryx-connectivity"
+  display_name               = "Adiryx Connectivity"
+  parent_management_group_id = azurerm_management_group.platform.id
+}
+
+resource "azurerm_management_group" "management" {
+  name                       = "adiryx-management"
+  display_name               = "Adiryx Management"
+  parent_management_group_id = azurerm_management_group.platform.id
+}
+
+#################################################
+# LANDING ZONES
+#################################################
+
+resource "azurerm_management_group" "landingzones" {
+  name                       = "adiryx-landingzones"
+  display_name               = "Adiryx Landing Zones"
+  parent_management_group_id = azurerm_management_group.adiryx.id
+}
+
+resource "azurerm_management_group" "prod" {
+  name                       = "adiryx-prod"
+  display_name               = "Adiryx Production"
+  parent_management_group_id = azurerm_management_group.landingzones.id
+}
+
+resource "azurerm_management_group" "nonprod" {
+  name                       = "adiryx-nonprod"
+  display_name               = "Adiryx Non-Production"
+  parent_management_group_id = azurerm_management_group.landingzones.id
+}
+
+resource "azurerm_management_group" "sandbox" {
+  name                       = "adiryx-sandbox"
+  display_name               = "Adiryx Sandbox"
+  parent_management_group_id = azurerm_management_group.adiryx.id
+}
+
+#################################################
+# SUBSCRIPTION ASSOCIATION
+#################################################
+
+resource "azurerm_management_group_subscription_association" "adiryx_soc_platform" {
+  management_group_id = azurerm_management_group.prod.id
+  subscription_id     = "/subscriptions/ff45ede4-ddf8-4818-9ab0-486b2d42d71e"
+}
